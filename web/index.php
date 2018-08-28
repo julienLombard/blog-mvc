8b164d6bbe81afa6511ee4fafc0f1f3082cde82c
MSI <MSI>
2018-08-28 22:41:28 +0200

    add Portfolio section in the home page, PostController.php and post.html.twig files

--- web/index.php
+++ web/index.php
@@ -15,14 +15,14 @@ $request = Request::createFromGlobals();
 $router = new Router($request);
 
 // Router Test
-$router->add(new Route("blog_show", "/blog/:id", ["id" => "\d+"], "blog", "show"));
-$router->add(new Route("home", "/", [], "Controller\HomeController", "show"));
+$router->add(new Route("show_post", "/post/:id", ["id" => "\d+"], "Controller\PostController", "showPost"));
+$router->add(new Route("home", "/", [], "Controller\HomeController", "index"));
 
 $route = $router->find();
 $response = $route->call($request, $router);
 $response->send();
 
-$database = new Database("localhost","dev_blog", "root","");
+// $database = new Database("localhost","dev_blog", "root","");
 // $post = new Post();
 // $post->setId(1);
 // $post->setTitle("mon titre remis à jour");
@@ -31,8 +31,8 @@ $database = new Database("localhost","dev_blog", "root","");
 // $post->setPublicationDate( new \DateTime());
 // $post->setModificationDate( new \DateTime());
 
-$manager = $database->getManager(Post::class);
-echo "<pre>";
+// $manager = $database->getManager(Post::class);
+// echo "<pre>";
 // $manager->update($post);
 
 // $post = $manager->find(1);
@@ -40,5 +40,5 @@ echo "<pre>";
 // $post->setModificationDate(new \DateTime());
 // $manager->update($post);
 
-$posts = $manager->findAll(0,2, "publicationDate", "DESC");
-var_dump($posts);
+// $posts = $manager->findAll(0,6, "publicationDate", "DESC");
+// var_dump($posts);
