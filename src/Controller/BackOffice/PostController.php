e9d0baa739f0b0723ac8c0fa5419bfbb1fd5ecdc
MSI <MSI>
2018-10-16 20:08:30 +0200

    Refactoring: Add FrontOffice, BackOffice Folder and PostController, CommentController, UserController Files. Update Yaml and Style.css Files

---
+++ src/Controller/BackOffice/PostController.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace Controller\BackOffice;
+
+use App\Controller;
+use App\ORM\Database;
+use Model\User;
+use Model\Post;
+// use Model\Comment;
+
+/**
+ * Class PostController
+ * @package Controller
+ */
+class PostController extends Controller
+{
+
+    /**
+    * @return \App\Response\Response
+    */
+    public function postsList()
+    {          
+        // get $_GET for page's number
+        $page = $this->getRequest()->getGet()['page'] ?? 1;
+
+        // get Database and PostManager
+        $database = $this->getDatabase();
+        $manager = $database->getManager(Post::class);
+        
+        // Pagination
+        $posts = $manager->getPagination($page, 0, 4, "publicationDate", "DESC", "", null);
+        
+        // View
+        return $this->render("adminPostsList.html.twig", [
+            "posts" => $posts, 
+            "page" => $page, 
+            "pageCount" => ceil($manager->countAllPost()/4)]);    
+
+    }
+
+    /**
+    * @param $id
+    * @return \App\Response\Response
+    */
+    public function showAdminPost($id)
+    {
+        $database = $this->getDatabase();
+        $manager = $database->getManager(Post::class);
+        $post = $manager->find($id);
+
+        return $this->render("adminPostShow.html.twig", ["post" => $post]);
+        
+    }
+
+    /**
+    * @return \App\Response\Response
+    */
+    public function createPostForm() 
+    {
+        // View: to create form
+        return $this->render("adminPostCreateForm.html.twig");
+        
+    }
+
+    /**
+    * @return \App\Response\Response
+    */
+    public function createPost() 
+    {
+        // get $_POST
+        $request = $this->getRequest();
+        $formPost = $request->getPost();
+        // get Database and Manager
+        $database = $this->getDatabase();
+        $manager = $database->getManager(Post::class);
+        
+        // set Post and insert it in database
+        $post = new Post();
+        $post->setTitle($formPost['title']);
+        $post->setContent($formPost['content']);
+        $post->setPublicationDate(new \DateTime());
+        $manager->insert($post);
+        
+        // Redirect route
+        return $this->redirect("admin_posts_list");
+    }
+
+    /**
+    * @param $id
+    * @return \App\Response\Response
+    */
+    public function showEditPost($id)
+    {
+        // Get Database, Manager and find Post
+        $database = $this->getDatabase();
+        $manager = $database->getManager(Post::class);
+        $post = $manager->find($id);
+        
+        // View
+        return $this->render("adminPostEdit.html.twig", ["post" => $post]);       
+    }
+
+    /**
+     * @param $id
+     * @return \App\Response\RedirectResponse
+     */
+    public function updatePost($id)
+    {
+        // get $_POST
+        $request = $this->getRequest();
+        $formPost = $request->getPost();
+        
+        // Get Database, Manager and find Post
+        $database = $this->getDatabase();
+        $manager = $database->getManager(Post::class);
+        $post = $manager->find($id);
+        
+        // Set and update Post
+        $post->setTitle($formPost['title']);
+        $post->setContent($formPost['content']);
+        $post->setModificationDate(new \DateTime());
+        $manager->update($post);
+        
+        // View
+        return $this->render("adminPostShow.html.twig", ["post" => $post]);
+        
+    }
+
+    /**
+     * @param $id
+     * @return \App\Response\RedirectResponse
+     */
+    public function confirmDeletePost($id)
+    {
+
+        // Get Database, Manager and find Post
+        $database = $this->getDatabase();
+        $manager = $database->getManager(Post::class);
+        $post = $manager->find($id);
+        
+        // View
+        return $this->render("adminPostConfirmDelete.html.twig", ["post" => $post]);
+        
+    }
+
+    /**
+     * @param $id
+     * @return \App\Response\RedirectResponse
+     */
+    public function deletePost($id)
+    {
+        // Get Database, Manager and find Post
+        $database = $this->getDatabase();
+        $manager = $database->getManager(Post::class);
+        $post = $manager->find($id);
+
+        // Delete Post
+        $manager->delete($post);
+        
+        // Redirect route
+        return $this->redirect("admin_posts_list");
+        
+    }
+}
---
+++ src/Controller/FrontOffice/PostController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Controller\FrontOffice;
+
+use App\Controller;
+use App\ORM\Database;
+use Model\Post;
+use Model\Comment;
+
+/**
+ * Class PostController
+ * @package Controller
+ */
+class PostController extends Controller
+{
+    /**
+    * @return \App\Response\Response
+    */
+    public function index()
+    {
+        // Get Database, Manager and find all Post
+        $database = $this->getDatabase();
+        $manager = $database->getManager(Post::class);
+        $posts = $manager->findAll(0,8, "publicationDate", "DESC","", null, null, null);
+
+        // View
+        return $this->render("home.html.twig", ["posts" => $posts]);
+    }
+
+    /**
+    * @return \App\Response\Response
+    */
+    public function postsList()
+    {          
+        // get $_GET for page's number
+        $page = $this->getRequest()->getGet()['page'] ?? 1;
+
+        // get Database and PostManager
+        $database = $this->getDatabase();
+        $manager = $database->getManager(Post::class);
+        
+        // Pagination
+        $posts = $manager->getPagination($page, 0, 4, "publicationDate", "DESC", "", null);
+        
+        // View
+        return $this->render("homePostsList.html.twig", [
+            "posts" => $posts, 
+            "page" => $page, 
+            "pageCount" => ceil($manager->countAllPost()/4)]);    
+
+    }
+
+    /**
+    * @param $id
+    * @return \App\Response\Response
+    */
+    public function showPost($id)
+    {
+        // get $_GET for page's number
+        $page = $this->getRequest()->getGet()['page'] ?? 1;
+
+        // Get Database
+        $database = $this->getDatabase();
+        // Get Post Manager and find Post
+        $postManager = $database->getManager(Post::class);
+    	$post = $postManager->find($id);
+
+        // Get Comments Manager and pagination
+        $commentManager = $database->getManager(Comment::class);      
+        $comments = $commentManager->getPagination($page, 0, 8, "publicationDate", "ASC", "postId", $id, "validate", "1");
+
+        // View
+        return $this->render("homePostShow.html.twig", [
+            "id" => $id, 
+            "post" => $post, 
+            "comments" => $comments, 
+            "page" => $this->getRequest()->getGet()['page'] ?? 1, 
+            "pageCount" => ceil($commentManager->countValidByPost($id)/8)]);
+    }
+
+    /**
+    * @return \App\Response\Response
+    */
+    public function sendMail()
+    {
+        // get $_POST
+        $post = $this->getRequest()->getPost();
+        $alert = 0;
+
+        // Contact Form
+        if (!empty($post['name'])    &&
+            !empty($post['email'])   &&
+            !empty($post['message'])) 
+        {
+            $name = $post['name'];
+            $email= $post['email'];
+            $message = $post['message'];
+            $alert = 1;
+
+            // Create the email and send the message
+            $to = 'julienlombard.Fr@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+            $email_subject = "Formulaire de contact du site web:  $name";
+            $email_body = "Vous avez reçu un nouveau message à partir du formulaire de contact de votre site Web.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nMessage:\n$message";
+            $headers = "De: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+            $headers .= "Répondre à: $email";	
+            mail($to,$email_subject,$email_body,$headers);
+
+        } elseif (empty($post['name'])    ||
+                empty($post['email'])   ||
+                empty($post['message'])) 
+        {
+                $alert = 2;
+        }
+
+        // Get Database, Manager and find all Post
+        $database = $this->getDatabase();
+        $manager = $database->getManager(Post::class);
+        $posts = $manager->findAll(0,8, "publicationDate", "DESC","", null, null, null);
+
+        // Redirect Route
+        return $this->render("home.html.twig", ["posts" => $post, "alert" => $alert]);
+    }
+}
